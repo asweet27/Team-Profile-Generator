de77352e0e92721ee68588c4d98e460b75d2d426
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-08-27 05:02:57 -0400

    completed code to get info

--- app.js
+++ app.js
@@ -5,31 +5,160 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.resolve(__dirname, "output", "team.html");
+
 
 const render = require("./lib/htmlRenderer");
 
+const teamArray = [];
+const teamIds = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function init() {
+    function nameTeam() {
+        inquirer.prompt([
+            {
+                type: "input"
+                message: "Welcome to the Team Profile Generator! Start by telling us the name of your team.",
+                name: "teamname"
+            }
+        ])
+            .then(function(data) {
+                const teamName = data.teamName
+                teamArray.push(teamName)
+                addManager();
+            });
+    }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    function addManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the team manager's name?",
+                name: "managerName",
+            },
+            {
+                type: "input",
+                message: "What is the team manager's ID number?",
+                name: "managerIdNumber",
+            },
+            {
+                type: "input",
+                message: "What is the team manager's email address?",
+                name: "managerEmail",
+            },
+            {
+                type: "input",
+                message: "What is the team manager's office number?",
+                name: "managerOfficeNumber",
+            },
+        ])
+        .then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArray.push(manager);
+            teamIds.push(answers.managerId)
+            buildTeam();
+            
+        });
+    }
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    function buildTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Would you like to add more team members to your site?",
+                name: "teamMemberAdd",
+                choices: [
+                    "Yes, add an Intern",
+                    "Yes, add an Engineer",
+                    "No, the team is complete"
+                ]
+            }
+        ])
+        .then(userInput => {
+            switch (userInput.teamMemberAdd) {
+                case "Yes, add an Intern":
+                    addIntern();
+                    break;
+                case "Yes, add an Engineer":
+                    addEngineer();
+                    break;
+                case "No, the team is complete":
+                    createTeamSite();
+                    break;
+            }
+        });
+    }
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the intern's name?",
+                name: "internName",
+            },
+            {
+                type: "input",
+                message: "What is the intern's email address?",
+                name: "internEmail",
+            },
+            {
+                type: "input",
+                message: "What is the intern's ID number?",
+                name: "internIdNumber",
+            },
+            {
+                type: "input",
+                message: "What school does the intern attend?",
+                name: "internSchool",
+            }
+        ])
+        .then(answers => {
+            const intern = new Intern(answers.internName, answers.internEmail, answers.internIdNumber, answers.internSchool);
+            teamArray.push(intern);
+            teamIds.push(answers.internIdNumber);
+            buildTeam();
+        });
+    }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "engineerName"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email address?",
+                name: "engineerEmail"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's ID number?",
+                name: "engineerIdNumber"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's Github username?",
+                name: "engineerGithub"
+            }
+        ])
+        .then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerEmail, answers.engineerIdNumber, answers.engineerGithub);
+            teamArray.push(engineer);
+            teamIds.push(answers.engineerIdNumber);
+            buildTeam();
+        });
+    }
+
+   function createTeamSite() {
+       fs.writeFileSync(outputPath, render(teamArray));
+       console.log("Success! Your team page has been rendered in the output folder!")
+   } 
+
+   nameTeam();
+
+
+}
+
+init();
